841dda434415fcf6506fc53036c6fb9d4fda9eb0
Lukas Erlacher <l.erlacher@gmail.com>
2013-01-31 19:28:59 +0100

    added polynomial regression algorithm for rangetables

---
+++ regression/regression.py
@@ -0,0 +1,179 @@
+import sys
+import math
+
+
+def main(argv):
+  if (len(argv) < 3):
+    print "Usage: %s <order> <files>" % (argv[0])
+    print "\n\tOrder indicates the desired order of the interpolated polynomial."
+    print "\tThe file parser expects an optional tab-separated header line and tab-separated columns."
+    print "\tThe first column will be used as X coordinate, all other columns as Y coordinates."
+    exit(0)
+
+  order = argv[1]
+
+  files = argv[2:]
+
+  for fn in files:
+    print "Processing file %s..." % (fn)
+    f = open(fn)
+    lno = 0
+    l = f.readline()
+
+    title = []
+    rec = []
+    row = l.split("\t")
+    if (not isNumber(row[0])):
+      title = row
+      lno += 1
+      l = f.readline()
+
+    while l != "":
+      srow = l.split("\t")
+      row = []
+      for s in srow:
+        if (isNumber(s)):
+          row.append(float(s))
+        else:
+          print "%s:%d - Could not parse \"%s\", skipping sample" % (fn,lno,s)
+          row.append(None)
+      lno += 1
+      l = f.readline()
+
+    ncols = len(rec[0])
+    result = []
+
+    for i in range(1,ncols):
+      xcol = rec[0]
+      ycol = rec[1]
+      set = zip(xcol, ycol)
+      set = filter( lambda x : (x[1] != None), set)
+
+      r = []
+      if len(title) >= i - 1:
+        print title[i]
+      else:
+        print "Column %d" % (i)
+
+      params = findCurve(set, order)
+
+      print params
+
+
+
+
+def isNumber(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+
+def findCurve(values, n):
+  if (n < 0 or values == None):
+    return None
+
+  if (n >= len(values)):
+    n = len(values)-1
+
+  coeffs = []
+  powers = []
+  powersums = []
+
+  # x^k for all x and all 0<=k<=2n
+  for i in range(len(values)):
+    x = values[i][0]
+    p = []
+    for j in range(2 * n + 1):
+      p.append(x**j)
+    powers.append(p)
+
+  # sum of x^k for all k
+  for i in range(2*n+1):
+    ps = 0
+    for j in range(len(values)):
+      ps += powers[j][i]
+    powersums.append(ps)
+
+  # coefficients of the linear system
+
+  for i in range(n):
+    cc = []
+    for k in range(n+1):
+      cc.append(powersums[i+k])
+
+    c = 0.0
+    for k in range(len(values)):
+      c += powers[k][i] * values[k][1]
+
+    cc.append(c)
+      
+    coeffs.append(cc)
+
+  #print "powers"
+  #print powers
+  #print "powersums"
+  #print powersums
+  #print "coeffs"
+  #print coeffs
+
+  return solveLinearSystem(coeffs)
+
+
+
+def solveLinearSystem(coefficients):
+  variableResultRows = []
+  for i in range(len(coefficients)):
+    variableResultRows.append(-1)
+
+  iOffset = 0
+  jOffset = 0
+  m = len(coefficients)
+  n = len(coefficients[0])-1
+
+  while jOffset < n:
+    firstNonZeroRow = -1
+    for i in range(iOffset, m):
+      if coefficients[i][jOffset] != 0:
+        firstNonZeroRow = i
+        break
+
+    if firstNonZeroRow != -1:
+      if firstNonZeroRow > iOffset:
+          swapRows(coefficients,iOffset,firstNonZeroRow)
+      factor = coefficients[iOffset][jOffset]
+      coefficients[iOffset][jOffset] = 1
+      for j in range(jOffset + 1, n +1):
+        coefficients[iOffset][j] /= factor
+      for i in range(0,m):
+        if (i != iOffset and coefficients[i][jOffset] != 0):
+          factor = coefficients[i][jOffset]
+          subMulRows(coefficients, i, iOffset, jOffset + 1, factor)
+      variableResultRows[jOffset] = iOffset
+      iOffset += 1
+
+    jOffset += 1
+
+  result = []
+  for i in range(0,m):
+    if variableResultRows[i] != -1:
+      result.append(coefficients[variableResultRows[i]][n])
+    else:
+      result.append(None)
+
+  result.reverse()
+
+  return result
+
+def subMulRows(coeffs, row1, row2, jOffset, factor):
+  for j in range(jOffset, len(coeffs[0])):
+    coeffs[row1][j] -= coeffs[row2][j] * factor
+
+def swapRows(coeffs, row1, row2):
+  temp = coeffs[row1]
+  coeffs[row1] = coeffs[row2]
+  coeffs[row2] = temp
+
+
+if __name__ == "__main__":
+  main(sys.argv)
